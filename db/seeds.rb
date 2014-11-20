a2f53973bfeecc1ae90f2cd504e3bf6714c44747
mklemme <mykklemme@gmail.com>
2014-11-19 16:52:32 -0800

    added 2 seed loos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+loos = Loo.create([
+  {
+    name: "Starbucks",
+    address: "123 Main Street. San Francisco, CA 00000",
+    rating: 0.5,
+    handicapped: true,
+    baby_changing: true,
+    key: true,
+    cost: "Customer only",
+    stall: "Single Occupancy",
+    shower: true,
+    toiletries: true,
+    venue: "Business"
+  },{
+    name: "Bay Stree Cafe",
+    address: "456 Bay Hay Nay Pay Second Street. San Francisco, CA 00000",
+    rating: 0.7,
+    handicapped: true,
+    baby_changing: false,
+    key: true,
+    cost: "Customer only",
+    stall: "Single Occupancy",
+    shower: true,
+    toiletries: false,
+    venue: "Business"
+  }
+  ])
