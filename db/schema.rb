40b783fdc6b9658e6ebd181f43920c8d55cbce46
mklemme <mykklemme@gmail.com>
2014-11-19 17:48:04 -0800

    added ratings model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119225707) do
+ActiveRecord::Schema.define(version: 20141120011053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 20141119225707) do
     t.boolean  "shower"
     t.boolean  "toiletries"
     t.string   "venue"
+    t.integer  "user_id"
+  end
+
+  create_table "ratings", force: true do |t|
+    t.integer  "rating"
+    t.string   "comment"
+    t.integer  "loo_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
