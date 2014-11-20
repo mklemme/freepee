e58644dd0994c40ae388b514c0acd7dff73646f6
George Syrop <george@syrop.co>
2014-11-19 16:34:59 -0800

    updated loo controller with new and create methods, added new loo route to no avail, committing in prep of loo model generation, potentially overwriting Myk's

--- app/controllers/loo_controller.rb
+++ app/controllers/loo_controller.rb
@@ -3,4 +3,11 @@ class LooController < ApplicationController
     @loos = Loo.all
     @users = User.all
   end
+
+  def new
+  end
+
+  def create
+  end
+
 end
--- spec/features/create_loos_spec.rb
+++ spec/features/create_loos_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 feature "CreateLoos", :type => :feature do
-  scenario "User is registering" do
+  scenario "visit new loo page" do
    
   #setup
-  visit new_user_registration_path
+  visit new_loo_path
 
   #exercise
-  fill_in "user[email]", with: "me@email.com"
-  fill_in "user[password]", with: "password"
-  click_button('Register')
+  # fill_in "name[email]", with: "me@email.com"
+  # fill_in "user[password]", with: "password"
+  # click_button('Register')
 
   #verify
-  expect(page).to have_text("Welcome! You have signed up successfully. me@email.com")
+  # expect(page).to have_text()
   # expect(session[:user_id]).to_not be_nil
 
   end 
