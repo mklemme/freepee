dfd7ee57a959c409e95eed7417514fefdea0b8ab
Kourosh Karimkhany <kouroshk@gmail.com>
2014-11-20 18:38:02 -0800

    Add validations to Loo model

--- app/models/loo.rb
+++ app/models/loo.rb
@@ -1,4 +1,13 @@
 class Loo < ActiveRecord::Base
   belongs_to :user
   has_many :ratings
+
+  validates :name, presence: true
+  validates :lat, presence: true
+  validates :long, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :postalCode, presence: true
+  validates :country, presence: true
 end
